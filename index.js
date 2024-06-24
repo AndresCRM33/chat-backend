d424e050f19612da3689a947e46f7098d1bb04c1
Andres Camilo Rios <acamilorm4556@gmail.com>
2024-06-24 00:35:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+
+const server = http.createServer();
+
+const io = require("socket.io")(server,{
+    cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+    console.log("se ha conectado un cliente")
+
+    socket.broadcast.emit("chat_message", {
+        usuario: "INFO",
+        mensaje: "Se ha conectado un nuevo usuario"
+    })
+
+    socket.on('typing', () => {
+        socket.broadcast.emit('typing', { usuario: socket.id });
+      });
+    
+    socket.on('stop_typing', () => {
+        socket.broadcast.emit('stop_typing', { usuario: socket.id });
+      });
+
+    socket.on("chat_message", (data) => {
+        console.log(data);
+        io.emit("chat_message", data);
+    })
+});
+
+server.listen(3000);
